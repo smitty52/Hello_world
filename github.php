75cf81c327d77c88c274e9cb5081b6db79920d30
Jorn Smit <44993167+smitty52@users.noreply.github.com>
2019-01-16 12:09:30 +0100

    Update github.php

--- github.php
+++ github.php
@@ -1,3 +1,4 @@
 <?php `git pull origin master`;
 # comment added for testing
 # another comment added after chmod 753 on script at 'push' side
+# another comment added after git commit on server side
